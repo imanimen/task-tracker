cc3d1f5c2eeb6f1872807d0e99f1017250c9c410
Iman Imen <imanimen81@gmail.com>
2022-11-01 22:42:33 +0330

    ADD single task component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import {useState} from "react";
 import Header from './components/Header'
 import Tasks from "./components/Tasks";
 
 function App() {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Doctors Appointment',
+            day: 'Feb 5th at 2:30pm',
+            reminder: false
+        },
+        {
+            id: 2,
+            text: 'Visit Parents',
+            day: 'Feb 6th at 4:30pm',
+            reminder: false
+        },
+        {
+            id: 3,
+            text: 'Finish Api Coding',
+            day: 'Feb 5th at 11am',
+            reminder: true
+        },
+    ])
     return ( 
         <div className = "container" >
             <Header title={'Task Tracker'} />
-            <Tasks/>
+            <Tasks tasks={tasks}/>
         </div>
     );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,34 +1,9 @@
-import React from "react";
+import Task from './Task'
 
-import { useState } from "react";
-
-// const tasks =
-
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 2:30pm',
-            reminder: false
-        },
-        {
-            id: 2,
-            text: 'Visit Parents',
-            day: 'Feb 6th at 4:30pm',
-            reminder: false
-        },
-        {
-            id: 3,
-            text: 'Finish Api Coding',
-            day: 'Feb 5th at 11am',
-            reminder: true
-        },
-    ])
-
+const Tasks = ({ tasks }) => {
     return (
         <>
-            {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+            { tasks.map((task) => (<Task key={task.id} task={task}/>)) }
         </>
     )
 }
