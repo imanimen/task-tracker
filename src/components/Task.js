3f03e79c578653c3de4139a2a56ef4ad2b119d38
Iman Imen <imanimen81@gmail.com>
2022-11-01 22:52:03 +0330

    ADD react-icon

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,10 @@
+import { FaTimes} from "react-icons/fa";
+
 const Task = ({ task }) => {
     return (
         <div className='task'>
             <h3>
-                {task.text}
+                {task.text} <FaTimes style={{color: 'red', cursor: 'pointer'}}/>
             </h3>
             <p>
                 {task.day}
