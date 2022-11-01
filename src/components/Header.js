41be65d43310fbc1f3788ff3c92265af3984ac59
Iman Imen <imanimen81@gmail.com>
2022-11-01 21:49:36 +0330

    FIX styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 const Header = ({ title }) => {
 	return (
 		<header className='header'>
-			<h1 style={headingStyle}>{title}</h1>
+			<h1>{title}</h1>
 			<button className='btn'>Add</button>
 		</header>
 	)
@@ -20,8 +20,8 @@ Header.propTypes = {
 }
 
 // css in js
-const headingStyle = {
-	color: 'red', backgroundColor: 'black'
-}
+// const headingStyle = {
+// 	color: 'red', backgroundColor: 'black'
+// }
 
 export default Header
