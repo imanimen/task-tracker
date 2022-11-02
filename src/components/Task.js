36754e47d6ec25cf808910783b5c88468157d188
Iman Imen <imanimen81@gmail.com>
2022-11-02 21:47:33 +0330

    ADD delete tasks and show message

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,7 +4,9 @@ const Task = ({ task, onDelete }) => {
     return (
         <div className='task'>
             <h3>
-                {task.text} <FaTimes style={{color: 'red', cursor: 'pointer'}} onClick={onDelete}/>
+                {task.text} <FaTimes
+                style={{color: 'red', cursor: 'pointer'}}
+                onClick={() => onDelete(task.id)}/>
             </h3>
             <p>
                 {task.day}
