f00f3c981b8d56f9317fcec9a3aa1231e2ff58d9
Iman Imen <imanimen81@gmail.com>
2022-11-10 14:30:37 +0330

    ADD addTask working

--- src/App.js
+++ src/App.js
@@ -25,7 +25,9 @@ function App() {
     ])
 
     const addTask = (task) => {
-        console.log(task)
+        const id = Math.floor(Math.random() * 10000 + 1)
+        const newTask = { id, ...task}
+        setTasks([...tasks, newTask])
     }
 
     const deleteTask = (id) => {
