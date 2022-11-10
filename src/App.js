87602b22266e5a92d63709376a5fad9db595a675
Iman Imen <imanimen81@gmail.com>
2022-11-10 13:43:38 +0330

    ADD task component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import {useState} from "react";
+import { useState } from "react";
 import Header from './components/Header';
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
-    const [tasks, setTasks] = useState([
-        {
+    const [tasks, setTasks] = useState([{
             id: 1,
             text: 'Doctors Appointment',
             day: 'Feb 5th at 2:30pm',
@@ -24,19 +24,20 @@ function App() {
         },
     ])
 
-    const deleteTask = (id)  => {
+    const deleteTask = (id) => {
         setTasks(tasks.filter((task) => task.id !== id))
     }
 
 
     // Toggle reminder
     const toggleReminder = (id) => {
-        setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+        setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder } : task))
     }
 
     return ( 
         <div className = "container" >
             <Header title={'Task Tracker'} />
+            <AddTask/>
             {tasks.length > 0 ? (
                 <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
                 ) : (
